5d3554321dbe6f93b023db79593a75892a595bd2
Nicolás López <nicolaslopezj@me.com>
2023-07-22 19:50:58 -0400

    try in the get pages from autoconfig

--- electron/pages/getPagesFromAutoConfig.ts
+++ electron/pages/getPagesFromAutoConfig.ts
@@ -2,25 +2,31 @@ import axios from 'axios'
 import {Config, getConfig} from '../app/config'
 
 export async function getPagesFormAutoConfig() {
-  const config = getConfig()
-  if (!config?.autoConfigString) {
-    throw new Error('No autoConfigString found in config')
+  try {
+    const config = getConfig()
+    if (!config?.autoConfigString) {
+      throw new Error('No autoConfigString found in config')
+    }
+
+    const base64decoded = Buffer.from(
+      config.autoConfigString,
+      'base64'
+    ).toString('utf-8')
+    const {url, token} = JSON.parse(base64decoded)
+
+    const {data} = await axios<{pages: Config['pages']}>({
+      url: url,
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    console.log(data)
+
+    return data.pages || []
+  } catch (error) {
+    console.error(error)
+    return []
   }
-
-  const base64decoded = Buffer.from(config.autoConfigString, 'base64').toString(
-    'utf-8'
-  )
-  const {url, token} = JSON.parse(base64decoded)
-
-  const {data} = await axios<{pages: Config['pages']}>({
-    url: url,
-    method: 'get',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-
-  console.log(data)
-
-  return data.pages || []
 }
