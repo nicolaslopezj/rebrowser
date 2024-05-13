11b80ad6c88b298edf3e333c8effd59feedde0af
Nicolás López <nicolaslopezj@me.com>
2024-05-13 09:41:32 -0400

    retry in get config

--- electron/pages/getPagesFromAutoConfig.ts
+++ electron/pages/getPagesFromAutoConfig.ts
@@ -1,23 +1,33 @@
 import axios from 'axios'
 import {Config, getConfig} from '../app/config'
 
-async function getAutoConfigFromString(autoConfigString: string) {
+async function getAutoConfigFromString(autoConfigString: string, retries = 0) {
   const base64decoded = Buffer.from(autoConfigString, 'base64').toString(
     'utf-8',
   )
   const {url, token} = JSON.parse(base64decoded)
 
-  const {data} = await axios<{pages: Config['pages']}>({
-    url: url,
-    method: 'get',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  try {
+    const {data} = await axios<{pages: Config['pages']}>({
+      url: url,
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
 
-  console.log(data)
+    console.log(data)
 
-  return data.pages || []
+    return data.pages || []
+  } catch (error) {
+    console.error('Error getting auto config', error)
+    if (retries < 5) {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      console.log('Retrying...')
+      return getAutoConfigFromString(autoConfigString, retries + 1)
+    }
+    return []
+  }
 }
 
 export async function getPagesFormAutoConfig(): Promise<Config['pages']> {
